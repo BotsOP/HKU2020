b222a8d3ef941bd81cbb372e2aef01b35ca84dd8
MrSinga <62131291+MrSynga@users.noreply.github.com>
2020-11-21 00:42:15 +0100

    update platform

--- HKUGAMEJAM/Assets/Scripts/FallingThrough.cs
+++ HKUGAMEJAM/Assets/Scripts/FallingThrough.cs
@@ -16,17 +16,21 @@ public class FallingThrough : MonoBehaviour
 
     }
 
-    public void OnCollisionEnter2D(Collision2D other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             GetComponent<BoxCollider2D>().size += new Vector2(0f, decaySpeed);
+            Debug.Log("touchy");
         }
     }
 
-    public void OnCollisionExit2D(Collision2D other)
+    public void OnTriggerExit(Collider other)
     {
-        if (GetComponent<BoxCollider2D>().size.y <= 0f)
+        if (GetComponent<BoxCollider2D>().size.y <= 0.5f)
+        {
             GetComponent<BoxCollider2D>().size = new Vector2(1f, 1f);
+            Debug.Log("releas");
+        }
     }
 }
