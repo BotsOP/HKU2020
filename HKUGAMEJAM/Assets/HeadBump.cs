59d6cb41eff8ebde5aff17a6dc4dd8fce10ce5fe
DESKTOP-O1U6U1E\prutm <prutmaild@gmail.com>
2020-11-21 16:02:18 +0100

    Added jumpthrough clouds

---
+++ HKUGAMEJAM/Assets/HeadBump.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadBump : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "JumpThrough")
+        {
+            Debug.Log(collision.name);
+            collision.gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "JumpThrough")
+        {
+            Debug.Log("solidify");
+            collision.gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+        }
+    }
+}
--- HKUGAMEJAM/Assets/Scripts/PlayerController.cs
+++ HKUGAMEJAM/Assets/Scripts/PlayerController.cs
@@ -63,6 +63,7 @@ public class PlayerController : MonoBehaviour
                 rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
             }
         }
+
         else
         {
             if (rb.velocity.y < 0)
@@ -102,11 +103,6 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             jumpCount++;
         }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        
     }
 
 
