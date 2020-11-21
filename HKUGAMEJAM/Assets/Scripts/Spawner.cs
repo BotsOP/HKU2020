c0f761ca597b2b31da974952896c12b11929db4d
MrSinga <62131291+MrSynga@users.noreply.github.com>
2020-11-21 11:07:06 +0100

    level rename

---
+++ HKUGAMEJAM/Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject page;
+    public Vector2 loc;
+    float screenX, screenY;
+    public GameObject quad;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        MeshCollider c = quad.GetComponent<MeshCollider>();
+
+        for(int i = 0; i < 1; i++)
+        {
+            screenX = Random.Range(c.bounds.min.x, c.bounds.max.x);
+            screenY = Random.Range(c.bounds.min.y, c.bounds.max.y);
+
+            loc = new Vector2(screenX, screenY);
+
+            Instantiate(page, loc, page.transform.rotation);
+        }
+    }
+}
