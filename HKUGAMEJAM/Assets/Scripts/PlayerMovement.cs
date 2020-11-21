03c539ecb208a0b000c4a0ff987d0fe0c40818db
Mijs <mijs.tenhove@hotmail.com>
2020-11-21 14:52:10 +0100

    code animation

--- HKUGAMEJAM/Assets/Scripts/PlayerMovement.cs
+++ HKUGAMEJAM/Assets/Scripts/PlayerMovement.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed, jumpForce;
+    bool isright, isleft;
 
+
+
+    public Animator anim;
+   
     Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
     void Update()
     {
+        float movement = Input.GetAxis("Horizontal");
         if (Input.GetKey(KeyCode.A))
         {
-            var movement = Input.GetAxis("Horizontal");
+
             transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+            anim.SetBool("walking", true);
+            print("walk trued");
+
         }
         if (Input.GetKey(KeyCode.D))
         {
-            var movement = Input.GetAxis("Horizontal");
             transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+           
+            anim.SetBool("walking", true);
+            print("walk trued");
+
         }
 
         if (Input.GetKey(KeyCode.Space))
@@ -31,5 +44,16 @@ public class PlayerMovement : MonoBehaviour
             if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001)
                 rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
+
+        if (movement == 0)
+        {
+            anim.SetBool("walking", false);
+            print("walk false");
+
+        }
+       
+
+
+
     }
 }
