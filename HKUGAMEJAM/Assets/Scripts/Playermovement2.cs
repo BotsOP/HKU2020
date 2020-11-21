3e766187567d2f8889112d8e52426e687325e84f
MrSinga <62131291+MrSynga@users.noreply.github.com>
2020-11-21 13:54:16 +0100

    player fix

--- HKUGAMEJAM/Assets/Scripts/Playermovement2.cs
+++ HKUGAMEJAM/Assets/Scripts/Playermovement2.cs
@@ -17,18 +17,18 @@ public class Playermovement2 : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            var movement = Input.GetAxis("Horizontal");
+            var movement = Input.GetAxis("HorizontalArrow");
             transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            var movement = Input.GetAxis("Horizontal");
+            var movement = Input.GetAxis("HorizontalArrow");
             transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001)
+            if (Input.GetButtonDown("JumpArrow") && Mathf.Abs(rb.velocity.y) < 0.001)
                 rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
     }
