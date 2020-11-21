cd9fe9a7ee133a7db9af7cdaf750bbed05a42b7e
MrSinga <62131291+MrSynga@users.noreply.github.com>
2020-11-21 16:25:05 +0100

    killzone

---
+++ HKUGAMEJAM/Assets/Scripts/KillZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+            collision.transform.position = spawnPoint.position;
+    }
+}
