89a641cec80a22bac4cc391cde0752e1ea6a8efa
MrSinga <62131291+MrSynga@users.noreply.github.com>
2020-11-22 11:00:31 +0100

    PauseMenu

---
+++ HKUGAMEJAM/Assets/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if (GamePaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
